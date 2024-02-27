b73898878157c00129f83655ac5b9aff2c940c54
Kabelo Malaka <kabelo.malaka@magnabc.co.za>
2024-02-27 09:57:47 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Fragment } from 'react';
+import Counter from './components/Counter';
+import Header from './components/Header';
+import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
+import { useSelector } from 'react-redux';
+
+function App() {
+  const authSelector = useSelector(state => state.auth.isAuthenticated);
+  return (
+    <Fragment>
+      <Header />
+      {authSelector ? <UserProfile /> : <Auth />}
+      <Counter />
+    </Fragment>
+  );
+}
+
+export default App;
